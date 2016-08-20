2bfa8d8f628d5441112f99078b09b9efc952a979
Eve <evelisevazquez@gmail.com>
2016-08-20 15:59:21 -0300

    [Front] Header Menu

--- wp-content/themes/fimTheme/header.php
+++ wp-content/themes/fimTheme/header.php
@@ -23,31 +23,12 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
-
-	<div id="sidebar" class="sidebar">
-		<header id="masthead" class="site-header" role="banner">
-			<div class="site-branding">
-				<?php
-					twentyfifteen_the_custom_logo();
-
-					if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
-
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; ?></p>
-					<?php endif;
-				?>
-				<button class="secondary-toggle"><?php _e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
-			</div><!-- .site-branding -->
-		</header><!-- .site-header -->
-
-		<?php get_sidebar(); ?>
-	</div><!-- .sidebar -->
-
-	<div id="content" class="site-content">
+	<div id="page" class="hfeed site">
+		<header class="holder">
+			<div class="content-logo">
+				<img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/logo.png">
+			</div>
+			<div class="content-menu oswald">
+				<?php wp_nav_menu( array( 'menu' => 'header-menu' ) ); ?>
+			</div>
+		</header>
