8cb6652265cd1ac1e2d10246291fb4b49615fb5d
Eve <evelisevazquez@gmail.com>
2016-08-20 15:01:43 -0300

    Arrumando o modelo

--- wp-content/themes/fimTheme/header.php
+++ wp-content/themes/fimTheme/header.php
@@ -15,10 +15,11 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href='<?php bloginfo( 'template_directory' ); ?>/assets/css/style.css' rel='stylesheet'>
 	<!--[if lt IE 9]>
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/html5.js"></script>
 	<![endif]-->
-	<?php wp_head(); ?>
+	
 </head>
 
 <body <?php body_class(); ?>>
