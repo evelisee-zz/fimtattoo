19ea772a1c2cdce2eecdbc50052630b02a403f3d
Eve <evelisevazquez@gmail.com>
2016-08-20 17:27:27 -0300

    teste botão header collpase

--- wp-content/themes/fimTheme/footer.php
+++ wp-content/themes/fimTheme/footer.php
@@ -28,7 +28,8 @@
 
 </div><!-- .site -->
 
-<script scr="<?php bloginfo( 'template_directory' ); ?>/assets/js/plugins/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script scr="<?php bloginfo( 'template_directory' ); ?>/assets/js/plugins/bootstrap.js"></script>
 
 </body>
 </html>
